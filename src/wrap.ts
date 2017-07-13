14943192ee89e777a6c670903b506326b8fe5fa6
Oliver Joseph Ash <oliverjash@gmail.com>
2017-07-13 22:14:59 +0100

    Add Session support

--- src/wrap.ts
+++ src/wrap.ts
@@ -2,17 +2,27 @@
 
 import * as express from 'express';
 
+import { getRequestSession } from './helpers/express';
 import * as MapHelpers from './helpers/map';
 import { Result } from './result';
 
-export const wrap = (fn: () => Result): express.RequestHandler => (
-    _req,
-    res,
-) => {
-    const result = fn();
+export const wrap = (
+    fn: (req: express.Request) => Result,
+): express.RequestHandler => (req, res) => {
+    const result = fn(req);
+    const headersStringDictionary = MapHelpers.toStringDictionary(
+        result.header.headers,
+    );
+    const sessionStringDictionary =
+        result.newSession !== undefined
+            ? MapHelpers.toStringDictionary(result.newSession)
+            : {};
+    const requestSession = getRequestSession(req);
+
+    requestSession.data = sessionStringDictionary;
     res
         .status(result.header.status)
-        .set(MapHelpers.toStringDictionary(result.header.headers))
+        .set(headersStringDictionary)
         .set(
             result.body.contentType !== undefined
                 ? { 'Content-Type': result.body.contentType }
